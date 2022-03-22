a6fdadbbe281937d4b5d928ce726f78e027294d6
warm113 <1135375289@qq.com>
2022-03-22 15:36:52 +0800

    2 files modified

--- src/views/CustomWrapper.vue
+++ src/views/CustomWrapper.vue
@@ -5,58 +5,57 @@
     animation="500"
     class="drag"
     @change="onChange"
+    @start="onStart"
     @end="onEnd"
     @sort="onSort"
-  >
+    filter=".undraggable"
+    ><!-- -->
     <slot></slot>
   </Draggable>
 </template>
 <script>
-import Draggable from 'vuedraggable'
+import Draggable from 'vuedraggable';
 
 export default {
   name: 'CustomWrapper',
   components: {
-    Draggable
+    Draggable,
   },
   // inject: ['dataSource'],
   props: ['dataSource'],
-  data () {
-    return {}
+  data() {
+    return {};
   },
   methods: {
-    onChange (evt) {
-      const data = this.dataSource
-      console.log('this.dataSource')
-      const index = evt.moved.newIndex
-      console.log('evt', evt)
+    onChange(evt) {
+      const data = this.dataSource;
+      console.log('this.dataSource');
+      const index = evt.moved.newIndex;
+      console.log('evt', evt);
       // 将你想要的值 传给父组件
       this.dataSource.handleDrag({
         id: data[index].id,
         before: data[index - 1] ? data[index - 1].id : '',
-        after: data[index + 1] ? data[index + 1].id : ''
-      })
+        after: data[index + 1] ? data[index + 1].id : '',
+      });
     },
-    onEnd (e) {
-      // console.log('end', e)
+    onEnd(e) {
+      const row = document.querySelectorAll('.ant-table-row');
+      console.log('end', e.item.classList[1]);
+      row.forEach((item) => {
+        if (item.classList[1] !== e.item.classList[1]) {
+          console.log('end 返回 false');
+          // item.classList.value = item.classList.value + ' undraggable';
+          return false;
+        }
+      });
     },
-    onSort (e) {
-      // console.log('sort', e)
-      // const dragItem = this.dataSource.splice(e.oldIndex, 1)
-      // this.dataSource.splice(e.newIndex, 1, dragItem)
-      console.log(this.$parent.$parent.$parent)
-      // const data = this.dataSource
-      // const index = evt.newIndex
-      // this.$parent.handleDrag({
-      //   // id: data[index].id,
-      //   // before: data[index - 1] ? data[index - 1].id : '',
-      //   // after: data[index + 1] ? data[index + 1].id : ''
-      //   newIndex: e.newIndex,
-      //   oldIndex: e.oldIndex
-      // })
-    }
-  }
-}
+    onSort(e) {},
+    onStart(e) {
+      console.log('start', e);
+    },
+  },
+};
 </script>
 
 <style lang="less" scoped>
